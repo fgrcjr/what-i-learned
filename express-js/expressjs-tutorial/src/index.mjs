6bc5c2a62a22ab4635029ae7c335d247c2d0aec8
iggy <fgrcjr@gmail.com>
2024-05-15 23:37:34 +0800

    add middleware and apply to GET, PUT, PATCH, and DELETE request

--- express-js/expressjs-tutorial/src/index.mjs
+++ express-js/expressjs-tutorial/src/index.mjs
@@ -5,6 +5,26 @@ const app = express()
 // Register Middleware
 app.use(express.json())
 
+// Sample Middleware function
+const loggingMiddleware = (request, reponse, next) => {
+    console.log(`${request.method} - ${request.url}`)
+    next()
+}
+
+const resolveIndexByUserId = (request, response, next) => {
+    const { body, params:{ id } } = request
+    const parseId = parseInt(id)
+    if (isNaN(parseId)) return response.sendStatus(400)
+
+    const findUserIndex = sampleUsers.findIndex((user) => user.id === parseId)
+    if(findUserIndex === -1) return response.sendStatus(404)
+    request.findUserIndex = findUserIndex
+    next()
+}
+
+// Always register the middleware before all endpoints
+app.use(loggingMiddleware)
+
 // Assuming that env has a port else use 3000
 const PORT = process.env.PORT || 3000
 
@@ -21,6 +41,21 @@ const sampleUsers = [
 // GET Requests
 // Use '/api/{}'
 // to get status "response.status(200)"
+app.get('/', 
+
+(request, response,next) => {
+    console.log("test 1")
+    next()
+},
+(request, response,next) => {
+    console.log("test 2")
+    next()
+}
+
+, (request, response) => {
+    return response.status(201).send({ msg: "Hello"})
+})
+
 app.get('/api/users', (request, response) => {
     // Query Params
     // sample_1: ?key=value&key2=value2
@@ -55,59 +90,89 @@ app.post('/api/users', (request, response) => {
 
 // Route Params
 // use console.log(request,params) to check what can be used as params
-app.get('/api/users/:id', (request, response) => {
-    // create validation
-    console.log(request.params)
-    const parseId = parseInt(request.params.id)
-    console.log(parseId)
+// app.get('/api/users/:id', (request, response) => {
+//     // create validation
+//     console.log(request.params)
+//     const parseId = parseInt(request.params.id)
+//     console.log(parseId)
 
-    if(isNaN(parseId)) return response.status(400).send({ msg: "Bad request. Invalid ID."})
+//     if(isNaN(parseId)) return response.status(400).send({ msg: "Bad request. Invalid ID."})
 
-    const findUser = sampleUsers.find((user) => user.id === parseId)
+//     const findUser = sampleUsers.find((user) => user.id === parseId)
+//     if (!findUser) return response.sendStatus(404)
+//     return response.send(findUser)
+// })
+
+// apply middleware for GET Request, Route Params
+app.get('/api/users/:id', resolveIndexByUserId, (request, response) => {
+    const { findUserIndex } = request
+    const findUser = sampleUsers[findUserIndex]
     if (!findUser) return response.sendStatus(404)
     return response.send(findUser)
 })
 
+
 // PUT Request
 // Update entire entity
-app.put('/api/users/:id', (request, response) => {
-    const { body, params:{ id } } = request
-    const parseId = parseInt(id)
-    if (isNaN(parseId)) return response.sendStatus(400)
+// app.put('/api/users/:id', (request, response) => {
+//     const { body, params:{ id } } = request
+//     const parseId = parseInt(id)
+//     if (isNaN(parseId)) return response.sendStatus(400)
 
-    const findUserIndex = sampleUsers.findIndex((user) => user.id === parseId)
-    if(findUserIndex === -1) return response.sendStatus(404)
+//     const findUserIndex = sampleUsers.findIndex((user) => user.id === parseId)
+//     if(findUserIndex === -1) return response.sendStatus(404)
 
-    sampleUsers[findUserIndex] = { id: parseId, ...body}
-    return response.sendStatus(200)
+//     sampleUsers[findUserIndex] = { id: parseId, ...body}
+//     return response.sendStatus(200)
+// })
+
+// apply middleware for PUT Request
+app.put('/api/users/:id', resolveIndexByUserId, (request, response) => {
+    const { body, findUserIndex } = request
+    sampleUsers[findUserIndex] = { id: sampleUsers[findUserIndex].id, ...body}
+    return response.sendStatus(200) 
 })
 
 // PATCH Request
 // Updates specific entity
-app.patch('/api/users/:id', (request, response) => {
-    const { body, params:{ id } } = request
-    const parseId = parseInt(id)
-    if (isNaN(parseId)) return response.sendStatus(400)
+// app.patch('/api/users/:id', (request, response) => {
+//     const { body, params:{ id } } = request
+//     const parseId = parseInt(id)
+//     if (isNaN(parseId)) return response.sendStatus(400)
 
-    const findUserIndex = sampleUsers.findIndex((user) => user.id === parseId)
-    if(findUserIndex === -1) return response.sendStatus(404)
+//     const findUserIndex = sampleUsers.findIndex((user) => user.id === parseId)
+//     if(findUserIndex === -1) return response.sendStatus(404)
 
-    // Take all the current values of sampleUsers by ...sampleUsers
-    // Then request body add the value on ...body to override value of sampleUsers
+//     // Take all the current values of sampleUsers by ...sampleUsers
+//     // Then request body add the value on ...body to override value of sampleUsers
+//     sampleUsers[findUserIndex] = { ...sampleUsers[findUserIndex], ...body}
+//     return response.sendStatus(200)
+// })
+
+// apply middleware for PATCH Request
+app.patch('/api/users/:id', resolveIndexByUserId,(request, response) => {
+    const { body, findUserIndex } = request
     sampleUsers[findUserIndex] = { ...sampleUsers[findUserIndex], ...body}
     return response.sendStatus(200)
 })
 
 // DELETE Request
-app.delete('/api/users/:id',(request, response) => {
-    const { params: { id }} = request
-    const parseId = parseInt(id)
-    if (isNaN(parseId)) return response.sendStatus(400)
+// app.delete('/api/users/:id',(request, response) => {
+//     const { params: { id }} = request
+//     const parseId = parseInt(id)
+//     if (isNaN(parseId)) return response.sendStatus(400)
 
-    // get index then splice
-    const findUserIndex = sampleUsers.findIndex((user) => user.id === parseId)
-    if (findUserIndex === -1) return response.sendStatus(404)
+//     // get index then splice
+//     const findUserIndex = sampleUsers.findIndex((user) => user.id === parseId)
+//     if (findUserIndex === -1) return response.sendStatus(404)
     
+//     sampleUsers.splice(findUserIndex, 1)
+//     return response.sendStatus(200)
+// })
+
+// apply middleware for DELETE Request
+app.delete('/api/users/:id',(request, response) => {
+    const { findUserIndex } = request
     sampleUsers.splice(findUserIndex, 1)
     return response.sendStatus(200)
 })
