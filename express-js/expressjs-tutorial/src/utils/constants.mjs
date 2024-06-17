e26bc1eba0b395a939a0f5608e0ba853533c1d00
iggy <fgrcjr@gmail.com>
2024-06-17 23:19:31 +0800

    Revert "update sample registration form"
    This reverts commit 8f9854f003eff8fc4ccb511365ff065c75798610.

--- express-js/expressjs-tutorial/src/utils/constants.mjs
+++ express-js/expressjs-tutorial/src/utils/constants.mjs
@@ -1,9 +1,9 @@
 export const sampleUsers = [
-    { id: 1, username: "wee", displayName: "Weee", password: "test123" },
-    { id: 2, username: "bebe", displayName: "Bebe", password: "test1234" },
-    { id: 3, username: "test", displayName: "Testing", password: "test13"  },
-    { id: 4, username: "duck", displayName: "Duck", password: "test23" },
-    { id: 5, username: "turtle", displayName: "Turtle", password: "test12" },
-    { id: 6, username: "8bitdo", displayName: "8BitDo", password: "test1235" },
-    { id: 7, username: "nsw", displayName: "Switch", password: "test" },
+    { id: 1, username: "wee", displayName: "Weee" },
+    { id: 2, username: "bebe", displayName: "Bebe" },
+    { id: 3, username: "test", displayName: "Testing" },
+    { id: 4, username: "duck", displayName: "Duck" },
+    { id: 5, username: "turtle", displayName: "Turtle" },
+    { id: 6, username: "8bitdo", displayName: "8BitDo" },
+    { id: 7, username: "nsw", displayName: "Switch" },
 ]
