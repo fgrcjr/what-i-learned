4b11cd60a0e6d26f7952d7bb9a88384ae8294076
ferds <fgrcjr@gmail.com>
2024-06-24 15:56:55 +0800

    sample type annotate route params by adding dtos folder for 3rd argument of Request and query params on 4th argument

--- express-js/expressjs-typescript-tutorial/src/handlers/users.ts
+++ express-js/expressjs-typescript-tutorial/src/handlers/users.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { CreateUserDto } from "../dtos/CreateUser.dto";
+import { CreateUserQueryParams } from "../types/query-params";
 
 export function getUsers(request: Request, response: Response){
     response.send([])
@@ -6,4 +8,8 @@ export function getUsers(request: Request, response: Response){
 
 export function getUserById(request: Request, response: Response){
     response.send({})
+}
+
+export function createUser(request: Request<{}, {}, CreateUserDto, CreateUserQueryParams>, response: Response){
+    request.query.loginAfterCreate
 }
