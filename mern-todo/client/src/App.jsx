86e45334b5d569279534cf02cba0c9f2855dd11a
ferds <fgrcjr@gmail.com>
2024-06-26 17:56:11 +0800

    connect backend and frontend

--- mern-todo/client/src/App.jsx
+++ mern-todo/client/src/App.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react"  
+
 export default function App() {
+
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    async function getTodos(){
+      const res = await fetch("/api/todos")
+      const todos = await res.json()
+
+      setMessage(todos.msg)
+    }
+    getTodos()
+  }, [])
+
   return (
     <main className="container">
-      <hi>Hwow project</hi>
+      <hi>wow project</hi>
+      {message && <p>{message}</p>}
     </main>
   );
 }
