b3a164e1c0910abcd3df3a8760e27644712f004a
ferds <fgrcjr@gmail.com>
2024-07-04 11:16:26 +0800

    add learning react

---
+++ learning-ReactJS/sample_react/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">MyNavbar</div>
+        <div className="space-x-4">
+          <a href="#" className="text-white">Home</a>
+          <a href="#" className="text-white">About</a>
+          <a href="#" className="text-white">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
