9c6b38474d0f50060a9060ce81854ac53668bc6a
ferds <fgrcjr@gmail.com>
2024-07-19 16:45:57 +0800

    update screen record code

--- javascript/screen-record/app.js
+++ javascript/screen-record/app.js
@@ -1,9 +1,10 @@
+// access screen and audio streams
 const startBtn = document.getElementById("btn");
 const downloadLink = document.getElementById("link");
 let blob = null;
 let videoStream = null;
 
-startBtn.addEventListener("click", startScreenCapturing);
+startBtn.addEventListener("click", startScreenCapture);
 
 async function startScreenCapture() {
     if (!navigator.mediaDevices.getDisplayMedia) {
@@ -38,4 +39,79 @@ async function startScreenCapture() {
       console.error(error);
       alert(error);
    }
-  }
+}
+
+
+function recordStream(stream) {
+  countDown();
+    const mediaRecorder = new MediaRecorder(stream, {
+      mimeType: "video/webm; codecs=vp8,opus",
+  });
+
+    const recordedChunks = [];
+    mediaRecorder.addEventListener("dataavailable", (e) =>
+      recordedChunks.push(e.data)
+  );
+
+  stream.getVideoTracks()[0].addEventListener("ended", () => {
+    mediaRecorder.stop();
+    stream.getAudioTracks()[0].stop();
+  });
+
+    mediaRecorder.addEventListener("stop", () => {
+    createVideoBlob(recordedChunks);
+    showRecordedVideo(blob);
+ });
+
+  setTimeout(() => mediaRecorder.start(), 4000);
+}
+
+function countDown() {
+  const countDownElement = document.getElementById("countdown");
+  countDownElement.style.display = "grid";
+  let count = 3;
+
+  function reduceCount() {
+    countDownElement.textContent = count;
+    count--;
+
+    if (count >= 0) {
+      setTimeout(reduceCount, 1000);
+ } else {
+      countDownElement.style.display = "none";
+ }
+ }
+
+  reduceCount();
+}
+
+function createVideoBlob(recordedChunks) {
+  blob = new Blob(recordedChunks, {
+    type: recordedChunks[0].type,
+ });
+}
+
+function showRecordedVideo() {
+  const video = document.getElementById("video");
+  video.src = URL.createObjectURL(blob);
+  calculateVideoDuration(video);
+}
+
+function calculateVideoDuration(videoElement) {
+  videoElement.addEventListener("loadedmetadata", () => {
+    if (videoElement.duration === Infinity) {
+      videoElement.currentTime = 1e101;
+      videoElement.addEventListener("timeupdate",() => {
+          videoElement.currentTime = 0;
+      },{ once: true });
+    }
+ });
+}
+
+
+downloadLink.addEventListener("click", () => {
+  downloadLink.href = URL.createObjectURL(blob);
+  const fileName = prompt("What is the name of your video?");
+  downloadLink.download = `${fileName}.webm`;
+  downloadLink.type = "video/webm";
+});
